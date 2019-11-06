4a12ca1c2c9e4a92400b62988f318b5f149f738b
edtheshed <edward.rixon@codurance.com>
2019-11-06 11:39:03 +0000

    Position added

--- src/main/java/e/rixon/kata/Rover.java
+++ src/main/java/e/rixon/kata/Rover.java
@@ -3,30 +3,32 @@ package e.rixon.kata;
 import static e.rixon.kata.Direction.*;
 
 public class Rover {
-    private int xPosition;
-    private int yPosition;
+    private Position position;
     private Direction direction;
 
     public Rover(int x, int y, Direction direction) {
-        this.xPosition = x;
-        this.yPosition = y;
+        this(new Position(x, y), direction);
+    }
+
+    public Rover(Position position, Direction direction) {
+        this.position = position;
         this.direction = direction;
     }
 
     public void move() {
         if (NORTH.equals(direction))
-            yPosition++;
+            position.setY(position.getY()+1);
         if (SOUTH.equals(direction))
-            yPosition--;
+            position.setY(position.getY()-1);
         if (EAST.equals(direction))
-            xPosition++;
+            position.setX(position.getX()+1);
         if (WEST.equals(direction))
-            xPosition--;
+            position.setX(position.getX()-1);
     }
 
     @Override
     public String toString() {
-        return String.format("%s %s %s", xPosition, yPosition, direction);
+        return String.format("%s %s %s", position.getX(), position.getY(), direction);
     }
 
 }
