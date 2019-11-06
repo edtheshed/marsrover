72a4f5cd7055f74969cdf10dedfc7f44fcfa05b3
edtheshed <edward.rixon@codurance.com>
2019-11-06 12:12:46 +0000

    map of direction and vector

--- src/main/java/e/rixon/kata/Rover.java
+++ src/main/java/e/rixon/kata/Rover.java
@@ -1,10 +1,17 @@
 package e.rixon.kata;
 
+import java.util.Map;
+
 import static e.rixon.kata.Direction.*;
 
 public class Rover {
     private Position position;
     private Direction direction;
+    private final Map<Direction, PositionVector> positionVectorMap = Map.of(
+            NORTH, new PositionVector(0, 1),
+            SOUTH, new PositionVector(0, -1),
+            EAST, new PositionVector(1, 0),
+            WEST, new PositionVector(-1, 0));
 
     public Rover(Position position, Direction direction) {
         this.position = position;
@@ -12,22 +19,37 @@ public class Rover {
     }
 
     public void move() {
-        if (NORTH.equals(direction))
-            this.position = position.move(0, 1);
-        if (SOUTH.equals(direction))
-            this.position = position.move(0, -1);
-        if (EAST.equals(direction))
-            this.position = position.move(1, 0);
-        if (WEST.equals(direction))
-            this.position = position.move(-1, 0);
-
+        for(Map.Entry<Direction, PositionVector> directionVector: positionVectorMap.entrySet()) {
+            Direction direction = directionVector.getKey();
+            if(direction.equals(this.direction)) {
+                PositionVector vector = directionVector.getValue();
+                this.position = position.move(vector.getxModifier(), vector.getyModifier());
+            }
+        }
     }
 
     @Override
     public String toString() {
         return String.format("%s %s %s", position.getX(), position.getY(), direction);
     }
+}
 
+class PositionVector {
+    private final int xModifier;
+    private final int yModifier;
+
+    PositionVector(int xModifier, int yModifier) {
+        this.xModifier = xModifier;
+        this.yModifier = yModifier;
+    }
+
+    public int getxModifier() {
+        return xModifier;
+    }
+
+    public int getyModifier() {
+        return yModifier;
+    }
 }
 
 enum Direction {
