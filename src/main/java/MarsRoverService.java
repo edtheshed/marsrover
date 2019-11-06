1da230ce5bfb97a6426df78f324fe018d84a8aa6
edtheshed <edward.rixon@codurance.com>
2019-11-06 10:45:59 +0000

    added moving to rover

--- src/main/java/MarsRoverService.java
+++ src/main/java/MarsRoverService.java
@@ -1,15 +1,28 @@
 public class MarsRoverService {
+    private int xPosition;
+    private int yPosition;
+    private String direction;
+
     public MarsRoverService(int xGridSize, int yGridSize) {
     }
 
     public void setRover(int x, int y, String direction) {
+        this.xPosition = x;
+        this.yPosition = y;
+        this.direction = direction;
     }
 
     public void moveRover(String moves) {
-        throw new UnsupportedOperationException();
+        //char[] moveArray = moves.toCharArray();
+//        for (char move : moveArray) {
+            if (direction == "N")
+                yPosition++;
+            if (direction == "S")
+                yPosition--;
     }
 
+
     public String getOutput() {
-        return "1 1 N";
+        return String.format("%s %s %s", xPosition, yPosition, direction);
     }
 }
--- src/test/java/MarsRoverServiceTest.java
+++ src/test/java/MarsRoverServiceTest.java
@@ -5,9 +5,26 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class MarsRoverServiceTest {
 
     @Test
-    void placing_a_rover_and_get_output_should_return_the_rover_location_and_direction() {
-        MarsRoverService service = new MarsRoverService(10, 10);
-        service.setRover(1, 1, "N");
-        assertEquals("1 1 N", service.getOutput());
+    void placing_a_rover_and_get_output_should_return_the_rover_location() {
+        MarsRoverService service1 = new MarsRoverService(10, 10);
+        service1.setRover(1, 1, "N");
+        assertEquals("1 1 N", service1.getOutput());
+
+        MarsRoverService service2 = new MarsRoverService(10, 10);
+        service2.setRover(2, 2, "W");
+        assertEquals("2 2 W", service2.getOutput());
+    }
+
+    @Test
+    void moving_a_placed_rover_and_get_output_should_return_new_location() {
+        MarsRoverService service1 = new MarsRoverService(10, 10);
+        service1.setRover(1, 1, "N");
+        service1.moveRover("M");
+        assertEquals("1 2 N", service1.getOutput());
+
+        MarsRoverService service2 = new MarsRoverService(10, 10);
+        service2.setRover(2, 2, "S");
+        service2.moveRover("M");
+        assertEquals("2 1 S", service2.getOutput());
     }
 }
